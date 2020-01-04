db12e7b3c6dbe09bfebec03d3830c35ad8207412
Johnson Joseph <jhonsonj88@gmail.com>
2020-01-04 08:29:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 let main = document.querySelector('main#main')
 main.remove()
 
-let newHeader = document.createElement('hi')
+let newHeader = document.createElement('h1')
 newHeader.id = 'victory'
 newHeader.innerHTML = "Johnson is the Champion"
 document.body.appendChild(newHeader)

8f56d09bc13620346cc9eefbe22c9f7ab05e2bd5
Johnson Joseph <jhonsonj88@gmail.com>
2020-01-04 08:37:08 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+let main = document.querySelector("main#main")
+    main.remove()
+
+    let newHeader = document.createElement('h1')
+    newHeader.id = "victory"
+    newHeader.innerHTML = "Johnson is the champion"
+    document.body.appendChild(newHeader)

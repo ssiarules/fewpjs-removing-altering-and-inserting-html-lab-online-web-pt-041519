feed9b200d09bf549f8bd1946873af516705ba04
Johnson Joseph <jhonsonj88@gmail.com>
2020-01-04 08:28:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 // Write your code here!
 let main = document.querySelector('main#main')
 main.remove()
+
+let newHeader = document.createElement('hi')
+newHeader.id = 'victory'
+newHeader.innerHTML = "Johnson is the Champion"
+document.body.appendChild(newHeader)

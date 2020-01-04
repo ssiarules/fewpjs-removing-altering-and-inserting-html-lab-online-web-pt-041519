5640c95891ab3f3512660cf2c6b6bd0f618dbf20
Johnson Joseph <jhonsonj88@gmail.com>
2020-01-04 08:35:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // Write your code here!
-let main = document.querySelector('main#main')
-main.remove()
+let main = document.querySelector("main#main")
+    main.remove()
 
-let newHeader = document.createElement('h1')
-newHeader.id = "victory"
-newHeader.innerHTML = "Johnson is the Champion"
-document.body.appendChild(newHeader)
+    let newHeader = document.createElement('h1')
+    newHeader.id = "victory"
+    newHeader.innerHTML = "Micah is the champion"
+    document.body.appendChild(newHeader)
